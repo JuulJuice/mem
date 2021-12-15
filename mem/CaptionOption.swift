2977eed9bac011c13acf5ca86d88ad0ed588c393
Julian <taz38877@msn.com>
2021-12-15 09:14:24 -0500

    less go

---
+++ mem/CaptionOption.swift
@@ -0,0 +1,13 @@
+//
+//  CaptionOption.swift
+//  mem
+//
+//  Created by Julian Jackson on 12/14/21.
+//
+
+import Foundation
+
+struct CaptionOption{
+    var emoji:String
+    var caption:String
+}
--- mem/ViewController.swift
+++ mem/ViewController.swift
@@ -12,8 +12,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        topSegmentedControl.removeAllSegments()
+        bottomSegmentedControl.removeAllSegments()
+        
+        for choice in topChoices {
+            topSegmentedControl.insertSegment(withTitle: choice.emoji,
+                                             at: topChoices.count,
+                                             animated: false)
+        }
+        for choice in bottomChoices {
+            bottomSegmentedControl.insertSegment(withTitle: choice.emoji,
+                                             at: topChoices.count,
+                                             animated: false)
+        }
+        topSegmentedControl.selectedSegmentIndex = 0
+        bottomSegmentedControl.selectedSegmentIndex = 0
     }
-
-
+    
+    @IBAction func options(_ sender: Any) {
+        updateCaptions()
+        
+    }
+    func updateCaptions(){
+        let topIndex = topSegmentedControl.selectedSegmentIndex
+        let bottomIndex = bottomSegmentedControl.selectedSegmentIndex
+        topCaptionLabel.text = topChoices[topIndex].caption
+        bottomCaptionLabel.text = bottomChoices[bottomIndex].caption
+    }
+    var topChoices:[CaptionOption] = [CaptionOption(emoji: "🕶", caption: "You know what's cool?"), CaptionOption(emoji: "💥", caption: "You know what makes me mad?"), CaptionOption(emoji: "💕", caption: "You know what I love?")]
+    
+    var bottomChoices:[CaptionOption] = [CaptionOption(emoji: "🐱", caption: "Cats wearing hats"), CaptionOption(emoji: "🐕", caption: "Dogs carrying logs"), CaptionOption(emoji: "🐒", caption: "Monkeys being funky")]
+    
+    @IBOutlet weak var topCaptionLabel: UILabel!
+    @IBOutlet weak var bottomCaptionLabel: UILabel!
+    
+    @IBOutlet weak var topSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var bottomSegmentedControl: UISegmentedControl!
+    
+    
 }
 
